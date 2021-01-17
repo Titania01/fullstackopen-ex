882e2869773ae39876e2a1e0426b3333404772a9
Titania01 <adegbitebidmus@gmail.com>
2021-01-17 05:34:34 +0100

    update code base

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -65,23 +65,24 @@ import ReactDOM from "react-dom";
 //   );
 // };
 
-const Hello = () => {
+const Hello = (props) => {
   return (
     <div>
-      <p>Hello world</p>
+      <p>Hello {props.name}, you are {props.age} years old </p>
     </div>
   )
 }
 
 const App = () => {
+  const name = 'Peter'
+  const age = 10
 
   return (
     <div>
       <h1>Greetings </h1>
-      <Hello /> 
-      <Hello /> 
-      <Hello /> 
-      <Hello /> 
+      <Hello name="Harvey" age={26 + 10} /> 
+      <Hello name={name} age={age} /> 
+      <Hello name={name} age = {age} /> 
     </div>
   )
 }
