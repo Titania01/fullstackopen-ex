299c7262477c48e0f9953466fb8fc989163ae922
Titania01 <adegbitebidmus@gmail.com>
2021-02-08 11:04:06 +0100

    feat:ex-1.1

--- part1/courseinfo/src/components/Total.jsx
+++ part1/courseinfo/src/components/Total.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Total = ({total}) => {
+const Total = ({exercise1, exercise2, exercise3 }) => {
   return (
     <div>
-      {total}
+      <p>Number of exercises {exercise1 + exercise2 + exercise3} </p>
     </div>
   )
 }
